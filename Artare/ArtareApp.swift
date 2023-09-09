cc6fe0f6e92c23cb84cbf912bb6adc3b8c10129d
CuriousR82 <rosajeon82@gmail.com>
2023-09-09 17:54:17 -0400

    Initial Commit

---
+++ Artare/ArtareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtareApp.swift
+//  Artare
+//
+//  Created by Rosa Jeon on 2023-09-09.
+//
+
+import SwiftUI
+
+@main
+struct ArtareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
